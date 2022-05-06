cf29dfcec513c4868a14a6131f749067ce2bdcc5
Rimezin <96949196+Rimezin@users.noreply.github.com>
2022-05-06 19:34:54 -0400

    fix pwa

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-// import "semantic-ui-css/semantic.min.css";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -9,3 +9,8 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
